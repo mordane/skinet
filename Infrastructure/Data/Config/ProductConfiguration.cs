411ceedecbfb1002b4e2d71cd6743800de6d3250
Richard Hilton <richard@hopewellsoftwaresolutions.com>
2021-05-28 13:53:58 -0400

    repository

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
